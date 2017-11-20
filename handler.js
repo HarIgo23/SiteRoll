e1817bdd3717f96a75ec96913e5ee8fb592c61a2
Igorsky <d7hoxston@gmail.com>
2017-11-21 00:10:11 +0300

    Added class in out function

--- handler.js
+++ handler.js
@@ -1,6 +1,5 @@
 function startSpin(){
     var number=random();
-    console.log(number);
     out(number);
 }
 
@@ -9,31 +8,46 @@ function random(){
     var max=100;
     var numRand=min-0.5 +Math.random()*(max-min+1);
     numRand=Math.round(numRand);
+    console.log(numRand);
     if(numRand>=1&&numRand<=12)
         return 1;
-    else if(numRand>12&&numRand<=24)
+    else if(numRand<=24)
         return 2;
-    else if(numRand>24&&numRand<=36)
+    else if(numRand<=36)
         return 3;
-    else if(numRand>36&&numRand<=48)
+    else if(numRand<=48)
         return 4;
-    else if(numRand>48&&numRand<=58)
+    else if(numRand<=58)
         return 5;
-    else if(numRand>58&&numRand<=68)
+    else if(numRand<=68)
         return 6;
-    else if(numRand>68&&numRand<=78)
+    else if(numRand<=78)
         return 7;
-    else if(numRand>78&&numRand<=86)
+    else if(numRand<=86)
         return 8;
-    else if(numRand>86&&numRand<=94)
+    else if(numRand<=94)
         return 9;
-    else if(numRand>94&&numRand<=100)
+    else if(numRand<=100)
         return 10;
 }
 function out(num){
     var out='';
     out+='<img src="Image/';
     out+=num;
-    out+='.jpg" alt="">';
+    out+='.jpg" alt="" ';
+    switch(true){
+        case num >=1 && num<=4:
+            out+='class="common"';
+            break;
+        case num<=7:
+            out+='class="uncommon"';
+            break;
+        case num<=9:
+            out+='class="rare"';
+            break;
+        default:
+            out+='class="legendary"';
+    }
+    out+='>'
     document.getElementById('out').innerHTML=out;
 }
